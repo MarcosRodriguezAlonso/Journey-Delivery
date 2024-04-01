bdfde2de63537a8eef44c9204b3ed27bccee129b
Lorena Martínez <lmartmol@gmail.com>
2024-04-01 20:23:39 +0200

    Refactor inquierer questions and test requests

---
+++ src/requests.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { AxiosHttpClient } from "./http-client/axios-http.client.js";
+import { DeliveryCreateDto, DeliveryDeleteDto } from "./types/Delivery.js";
+import buildUrl from "./http-client/buildUrl/buildUrl.js";
+import { baseUrl } from "./config/apiPaths/apiPaths.js";
+
+const axiosHttpClient = new AxiosHttpClient(axios.create());
+const deliveriesUrl = buildUrl(baseUrl, "/deliveries");
+
+export const getDeliveries = async () => {
+  return await axiosHttpClient.get(deliveriesUrl);
+};
+
+export const createDelivery = async (deliveryCreateDto: DeliveryCreateDto) => {
+  return await axiosHttpClient.post(deliveriesUrl, deliveryCreateDto);
+};
+
+export const deleteDelivery = async ({ owner, week }: DeliveryDeleteDto) => {
+  const query = {
+    owner,
+    week: week.toString(),
+  };
+
+  const deliveriesUrlToDelete = buildUrl(baseUrl, "/deliveries", query);
+
+  return await axiosHttpClient.delete(deliveriesUrlToDelete);
+};
---
+++ src/types/Delivery.ts
@@ -0,0 +1,22 @@
+import { UUID } from "crypto";
+
+export type Urls = {
+  front: string;
+  back: string;
+};
+
+export type Delivery = {
+  id: UUID;
+  owner: string;
+  week: number;
+  hasPartner: boolean;
+  firstPartnerName?: string;
+  secondPartnerName?: string;
+  deliveryDate: Date;
+  frontUrls?: Urls;
+  backUrls?: Urls;
+};
+
+export type DeliveryCreateDto = Omit<Delivery, "id">;
+
+export type DeliveryDeleteDto = Pick<Delivery, "owner" | "week">;
